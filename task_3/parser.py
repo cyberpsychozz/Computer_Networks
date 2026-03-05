a89a36761ff7e3efbf6f21f921439c9f32af95a3
cyberpsychoz <nik631stal@mail.ru>
2026-03-05 12:49:21 +0700

    upd

--- task_3/parser.py
+++ task_3/parser.py
@@ -34,45 +34,45 @@ def set_search_parameters(page, city, vacancy):
     page.keyboard.press("Enter")
 
 def parse_vacancies(page, page_to_parse):
-     #parsing 
-        results = []
-        for pg in range(page_to_parse):
-            page.wait_for_load_state("networkidle")
+    #parsing 
+    results = []
+    for pg in range(page_to_parse):
+        page.wait_for_load_state("networkidle")
 
-            items = page.query_selector_all('div[data-qa="vacancy-serp__vacancy"]')
+        items = page.query_selector_all('div[data-qa="vacancy-serp__vacancy"]')
 
-            for item in items:
-                vacancy_el = item.query_selector('[data-qa="serp-item__title"]')
-                vacancy_name = vacancy_el.inner_text().strip() if vacancy_el else "N/A"
+        for item in items:
+            vacancy_el = item.query_selector('[data-qa="serp-item__title"]')
+            vacancy_name = vacancy_el.inner_text().strip() if vacancy_el else "N/A"
 
-                company_el = item.query_selector('[data-qa="vacancy-serp__vacancy-employer"]')
-                company_name = company_el.inner_text().strip() if company_el else "N/A"
-                
-                salary_el = item.query_selector('.compensation-labels--vwum2s12fQUurc2J span')
-                salary = salary_el.inner_text().strip() if salary_el else "N/A"
-
-                experience_el = item.query_selector('div[class*="magritte-tag__label"] span')
-                experience = experience_el.inner_text().strip() if experience_el else "N/A"
-
-                if salary == experience: salary = 'N/A'
-
-                results.append( {
-                    "Company name" : company_name, 
-                    "vacancy name" : vacancy_name,
-                    "expirience" : experience, 
-                    "salary" : salary
-                })
+            company_el = item.query_selector('[data-qa="vacancy-serp__vacancy-employer"]')
+            company_name = company_el.inner_text().strip() if company_el else "N/A"
             
-           
-            next_page_number = str(pg + 2)
-            next_button = page.locator(f'a[data-qa="pager-page"]:has-text("{next_page_number}")')
-            if next_button.count() > 0:
-                next_button.click()
-                page.wait_for_load_state("networkidle")
-            else:
-                print("Страницы кончились")
-                break
-        return results
+            salary_el = item.query_selector('.compensation-labels--vwum2s12fQUurc2J span')
+            salary = salary_el.inner_text().strip() if salary_el else "N/A"
+
+            experience_el = item.query_selector('div[class*="magritte-tag__label"] span')
+            experience = experience_el.inner_text().strip() if experience_el else "N/A"
+
+            if salary == experience: salary = 'N/A'
+
+            results.append( {
+                "Company name" : company_name, 
+                "vacancy name" : vacancy_name,
+                "expirience" : experience, 
+                "salary" : salary
+            })
+        
+        
+        next_page_number = str(pg + 2)
+        next_button = page.locator(f'a[data-qa="pager-page"]:has-text("{next_page_number}")')
+        if next_button.count() > 0:
+            next_button.click()
+            page.wait_for_load_state("networkidle")
+        else:
+            print("Страницы кончились")
+            break
+    return results
 
 def main():
     with sync_playwright() as p:
